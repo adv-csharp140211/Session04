5f19eafb3f498ea532ba9f5422d31978472d2815
Pashaie <pashaie@gmail.com>
2024-02-19 18:33:46 +0330

    .

--- App04/Form1.Designer.cs
+++ App04/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnEven = new Button();
+            listBox1 = new ListBox();
+            btnOdd = new Button();
+            buttonGT5 = new Button();
+            SuspendLayout();
+            // 
+            // btnEven
+            // 
+            btnEven.Location = new Point(12, 12);
+            btnEven.Name = "btnEven";
+            btnEven.Size = new Size(187, 23);
+            btnEven.TabIndex = 0;
+            btnEven.Text = "Even";
+            btnEven.UseVisualStyleBackColor = true;
+            btnEven.Click += btnEven_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 162);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(187, 274);
+            listBox1.TabIndex = 1;
+            // 
+            // btnOdd
+            // 
+            btnOdd.Location = new Point(12, 41);
+            btnOdd.Name = "btnOdd";
+            btnOdd.Size = new Size(187, 23);
+            btnOdd.TabIndex = 2;
+            btnOdd.Text = "Odd";
+            btnOdd.UseVisualStyleBackColor = true;
+            btnOdd.Click += btnOdd_Click;
+            // 
+            // buttonGT5
+            // 
+            buttonGT5.Location = new Point(12, 70);
+            buttonGT5.Name = "buttonGT5";
+            buttonGT5.Size = new Size(187, 23);
+            buttonGT5.TabIndex = 3;
+            buttonGT5.Text = "GT 5";
+            buttonGT5.UseVisualStyleBackColor = true;
+            buttonGT5.Click += buttonGT5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonGT5);
+            Controls.Add(btnOdd);
+            Controls.Add(listBox1);
+            Controls.Add(btnEven);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnEven;
+        private ListBox listBox1;
+        private Button btnOdd;
+        private Button buttonGT5;
     }
 }
--- App04/Form1.cs
+++ App04/Form1.cs
@@ -2,9 +2,94 @@ namespace App04
 {
     public partial class Form1 : Form
     {
+        List<int> numbers = new List<int>() { 2, 5, 6, 7, 9, 10, 12, 13 };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEven_Click(object sender, EventArgs e)
+        {
+            var result = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                var number = numbers[i];
+                if (number % 2 == 0)
+                {
+                    result.Add(number);
+                }
+            }
+
+            listBox1.Items.Clear();
+            foreach (var number in result)
+            {
+                listBox1.Items.Add(number.ToString());
+            }
+
+        }
+
+        private void btnOdd_Click(object sender, EventArgs e)
+        {
+            var result = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                var number = numbers[i];
+                if (number % 2 != 0)
+                {
+                    result.Add(number);
+                }
+            }
+
+            listBox1.Items.Clear();
+            foreach (var number in result)
+            {
+                listBox1.Items.Add(number.ToString());
+            }
+        }
+
+        private void buttonGT5_Click(object sender, EventArgs e)
+        {
+            var result = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                var number = numbers[i];
+                if (number > 5)
+                {
+                    result.Add(number);
+                }
+            }
+
+            listBox1.Items.Clear();
+            foreach (var number in result)
+            {
+                listBox1.Items.Add(number.ToString());
+            }
+        }
+
+
+        private void Xyz(Func<int, bool> predicate)
+        {
+            var result = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                var number = numbers[i];
+                if (predicate(number))
+                {
+                    result.Add(number);
+                }
+            }
+
+            listBox1.Items.Clear();
+            foreach (var number in result)
+            {
+                listBox1.Items.Add(number.ToString());
+            }
         }
     }
 }
