347e80d5c92a9a5c8491e1d98ffd6de2931c2d4e
Pashaie <pashaie@gmail.com>
2024-02-19 18:56:08 +0330

    .

--- App04/Form1.cs
+++ App04/Form1.cs
@@ -1,3 +1,5 @@
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace App04
 {
     public partial class Form1 : Form
@@ -16,80 +18,108 @@ namespace App04
 
         private void btnEven_Click(object sender, EventArgs e)
         {
-            var result = new List<int>();
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                var number = numbers[i];
-                if (number % 2 == 0)
-                {
-                    result.Add(number);
-                }
-            }
+            //var result = new List<int>();
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    var number = numbers[i];
+            //    if (number % 2 == 0)
+            //    {
+            //        result.Add(number);
+            //    }
+            //}
+
+            //listBox1.Items.Clear();
+            //foreach (var number in result)
+            //{
+            //    listBox1.Items.Add(number.ToString());
+            //}
+
+
+            //Xyz((int number) => number % 2 == 0);
+            //Xyz((number) => number % 2 == 0);
+            //Xyz(number => number % 2 == 0);
+
+            //lambda express
+            //lazy
+
+            var result = Xyz(n => n % 2 == 0);
 
             listBox1.Items.Clear();
-            foreach (var number in result)
-            {
-                listBox1.Items.Add(number.ToString());
-            }
+            listBox1.Items.AddRange(result.Cast<object>().ToArray());
 
         }
 
         private void btnOdd_Click(object sender, EventArgs e)
         {
-            var result = new List<int>();
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                var number = numbers[i];
-                if (number % 2 != 0)
-                {
-                    result.Add(number);
-                }
-            }
+            //var result = new List<int>();
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    var number = numbers[i];
+            //    if (number % 2 != 0)
+            //    {
+            //        result.Add(number);
+            //    }
+            //}
+
+            //listBox1.Items.Clear();
+            //foreach (var number in result)
+            //{
+            //    listBox1.Items.Add(number.ToString());
+            //}
+            var result = Xyz(x => x % 2 != 0);
 
             listBox1.Items.Clear();
-            foreach (var number in result)
-            {
-                listBox1.Items.Add(number.ToString());
-            }
+            listBox1.Items.AddRange(result.Cast<object>().ToArray());
         }
 
         private void buttonGT5_Click(object sender, EventArgs e)
         {
-            var result = new List<int>();
-            for (int i = 0; i < numbers.Count; i++)
-            {
-                var number = numbers[i];
-                if (number > 5)
-                {
-                    result.Add(number);
-                }
-            }
+            //var result = new List<int>();
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    var number = numbers[i];
+            //    if (number > 5)
+            //    {
+            //        result.Add(number);
+            //    }
+            //}
+
+            //listBox1.Items.Clear();
+            //foreach (var number in result)
+            //{
+            //    listBox1.Items.Add(number.ToString());
+            //}
+
+
+            Xyz((int x) => {
+                //
+                return x > 5;
+            });
+            Xyz((int x) => x > 5);
+            Xyz(x => x > 5);
+
+            var result = Xyz(x => x > 5);
 
             listBox1.Items.Clear();
-            foreach (var number in result)
-            {
-                listBox1.Items.Add(number.ToString());
-            }
+            listBox1.Items.AddRange(result.Cast<object>().ToArray());
         }
 
 
-        private void Xyz(Func<int, bool> predicate)
+        private List<int> Xyz(Func<int, bool> shart)
         {
             var result = new List<int>();
             for (int i = 0; i < numbers.Count; i++)
             {
                 var number = numbers[i];
-                if (predicate(number))
+                if (shart(number))
                 {
                     result.Add(number);
                 }
             }
 
-            listBox1.Items.Clear();
-            foreach (var number in result)
-            {
-                listBox1.Items.Add(number.ToString());
-            }
+            return result;           
         }
+
+        
     }
 }
